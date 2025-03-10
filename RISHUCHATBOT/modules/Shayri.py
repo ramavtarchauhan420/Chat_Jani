d4173427115a69917d30d13392187b6db94ffacd
ISTKHAR-king <istkharalam225@gmail.com>
2025-03-10 17:53:09 +0530

    Update Shayri.py

--- RISHUCHATBOT/modules/Shayri.py
+++ RISHUCHATBOT/modules/Shayri.py
@@ -71,10 +71,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "❍ 𝐒ᴜᴘᴘᴏʀᴛ ❍", url=f"https://t.me/ur_rishu_143"
+                        "❍ 𝐒ᴜᴘᴘᴏʀᴛ ❍", url=f"https://t.me/+pnDJxCG5VVphMTVl"
                     ),
                     InlineKeyboardButton(
-                        "❍ 𝐔ᴘᴅᴀᴛᴇ ❍", url=f"https://t.me/vip_robotz"
+                        "❍ 𝐔ᴘᴅᴀᴛᴇ ❍", url=f"https://t.me/THUNDERDEVS"
                     ),
                 ]
             ]
